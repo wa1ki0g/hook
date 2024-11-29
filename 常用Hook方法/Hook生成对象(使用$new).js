14728699a750f6e82a0af93d14a7f559581723a0
wa1 <70200814+wa1ki0g@users.noreply.github.com>
2024-11-29 15:13:25 +0800

    Create Hook生成对象(使用$new).js

---
+++ 常用Hook方法/Hook生成对象(使用$new).js
@@ -0,0 +1,4 @@
+const JavaString = Java.use('java.lang.String');
+var exampleString1 = JavaString.$new('Hello World, this is an example string in Java.');
+
+console.log('[+] exampleString1: ' + exampleString1);
