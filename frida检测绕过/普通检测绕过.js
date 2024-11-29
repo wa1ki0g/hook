a0b5c5954d23e5d58f30717096b4cef88422c2e6
wa1 <70200814+wa1ki0g@users.noreply.github.com>
2024-11-29 16:27:48 +0800

    Update 普通检测绕过.js

--- frida检测绕过/普通检测绕过.js
+++ frida检测绕过/普通检测绕过.js
@@ -1,3 +1,11 @@
+# 检测点说明：
+# gmain：Frida 使用 Glib 库，其中的主事件循环被称为 GMainLoop。在 Frida 中，gmain 表示 GMainLoop 的线程。
+# gdbus：GDBus 是 Glib 提供的一个用于 D-Bus 通信的库。在 Frida 中，gdbus 表示 GDBus 相关的线程。
+# gum-js-loop：Gum 是 Frida 的运行时引擎，用于执行注入的 JavaScript 代码。gum-js-loop 表示 Gum 引擎执行 JavaScript 代码的线程。
+# pool-frida：Frida 中的某些功能可能会使用线程池来处理任务，pool-frida 表示 Frida 中的线程池。
+# linjector 是一种用于 Android 设备的开源工具，它允许用户在运行时向 Android 应用程序注入动态链接库（DLL）文件。通过注入 DLL 文件，用户可以修改应用程序的行为、调试应用程序、监视函数调用等，这在逆向工程、安全研究和动态分析中是非常有用的。
+
+
 function replace_str() {
     var pt_strstr = Module.findExportByName("libc.so", 'strstr');
     var pt_strcmp = Module.findExportByName("libc.so", 'strcmp');
