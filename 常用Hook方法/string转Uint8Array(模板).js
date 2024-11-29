e913f24c7160b45f400322b44d7bc8ea4b1987df
wa1 <70200814+wa1ki0g@users.noreply.github.com>
2024-11-29 15:32:14 +0800

    Create string转Uint8Array(模板).js

---
+++ 常用Hook方法/string转Uint8Array(模板).js
@@ -0,0 +1,8 @@
+function stringToUint8Array(str){
+  var arr = [];
+  for (var i = 0, j = str.length; i < j; ++i) {
+    arr.push(str.charCodeAt(i));
+  }
+  var tmpUint8Array = new Uint8Array(arr);
+  return tmpUint8Array
+}
