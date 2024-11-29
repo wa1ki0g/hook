7d435249d5d236d9b5e82fc243f0b53dde98a95d
wa1 <70200814+wa1ki0g@users.noreply.github.com>
2024-11-29 15:33:14 +0800

    Create string转ArrayBuffer(模板).js

---
+++ 常用Hook方法/string转ArrayBuffer(模板).js
@@ -0,0 +1,8 @@
+function str2ab(str) {
+    var buf = new ArrayBuffer(str.length * 2); // 每个字符占用2个字节
+    var bufView = new Uint16Array(buf);
+    for (var i = 0, strLen = str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
